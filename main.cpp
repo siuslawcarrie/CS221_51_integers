46ea511057189ee16a3b5feebb0770805fef4915
siuslawcarrie <102423932+siuslawcarrie@users.noreply.github.com>
2024-05-19 09:25:44 -0700

    all working except mode, cleaned up code

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 using namespace std;
 
 const float SIZE = 51.0;
+
 //FUNCTION PROTOTYPES
 int random_num();//generate random number
 void integer_display_51(vector<int> random);//display 51 random integers
@@ -17,28 +18,26 @@ int main() {
     int mode; // variable to hold most frequent number in set
     float average; //variable for average of the integers
     vector<int> random_51;//vector for 51 random integers
-
+    cout << "Here are " << SIZE << " random integers:" << endl;
     for (int x = 1; x <= SIZE; x++) {//generate 51 integers
-    random_51.push_back(random_num());
+        random_51.push_back(random_num());
     }
-
-   integer_display_51(random_51);//display 51 integers
-
-
-   sort (random_51.begin(), random_51.end());//sorting inside the vector
+    integer_display_51(random_51);//display 51 integers
+    cout << "Here are " << SIZE << " random integers in numerical order:" << endl;
+    sort(random_51.begin(), random_51.end());//sorting inside the vector
     integer_display_51(random_51);//display 51 integers,sorted
 
-    cout<<"The median is: "<<random_51[26]<<endl;
+    cout << "The median is: " << random_51[26] << endl;
 
-   mode = findMode(vector<int> random); //call mode function
+//   mode = findMode(vector<int> random); //call mode function
 
-int sum = 0;
-for (int x = 0; x<SIZE; x++){
-    sum+=random_51[x];
-    //divide sum by size outside loop
-}
-average = sum/SIZE;
-cout<<fixed<<setprecision(2)<<"The average is: "<<average<<endl;
+    int sum = 0;
+    for (int x = 0; x < SIZE; x++) {
+        sum += random_51[x];
+        //divide sum by size outside loop
+    }
+    average = sum / SIZE;
+    cout << fixed << setprecision(2) << "The average is: " << average << endl;
 
 /* to find mode, loop through random 51, how many times have we seen each number, which one is the most, how many times have we seen that number
  * cout current most frequent:
@@ -48,19 +47,21 @@ cout<<fixed<<setprecision(2)<<"The average is: "<<average<<endl;
 */
 
 }
-int random_num(){
+
+int random_num() {
     int rand_num;
     rand_num = rand() % 20 + 1;
     return rand_num;
 }
 
-void integer_display_51(vector<int> random){
-    for (int x = 0; x<random.size(); x++){
-        cout<<random[x]<<" ";
+void integer_display_51(vector<int> random) {
+    for (int x = 0; x < random.size(); x++) {
+        cout << random[x] << " ";
 
     }
-    cout<<endl;
+    cout << endl;
 }
+
 //find mode
 int findMode(vector<int> random) //pass in vector's integers
 {
